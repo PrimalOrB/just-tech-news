c7017c2218ff040c90df46c97dc9e6b697ad817f
Andrew Ogilvie <primalorb@gmail.com>
2021-05-08 20:51:07 -0400

    initial setup

---
+++ models/User.js
@@ -0,0 +1,46 @@
+const { Model, DataTypes } = require( 'sequelize' );
+const sequelize = require( '../config/connection' );
+
+// create User model
+class User extends Model {}
+
+// define table columns and configuration
+User.init(
+    {
+        id: {
+            type: DataTypes.INTEGER,
+            allowNull: false,
+            primaryKey: true,
+            autoIncrement: true
+        },
+        username: {
+            type: DataTypes.STRING,
+            allowNull: false
+        },
+        email: {
+            type: DataTypes.STRING,
+            allowNull: false,
+            unique: true,
+            validate: {
+                isEmail: true
+            },
+        },
+        password: {
+            type: DataTypes.STRING,
+            allowNull: false,
+            validate: {
+                len: [4]
+            }
+        }
+    },
+    {
+        // table config options
+        sequelize, // imported sequelize connection
+        timestamps: false, // don't automatically create timestamp fields for createdAt / updatedAt
+        freezeTableName: true, // don't pluralize table name
+        underscored: true, // instead of camelcase
+        modelName: 'user' // lowercase for db
+    }
+);
+
+module.exports = User;
