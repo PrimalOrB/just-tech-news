a61f244047a25870862ec471e7351537ed764cb7
Andrew Ogilvie <primalorb@gmail.com>
2021-05-11 21:16:32 -0400

    add votes table and related functions

--- models/index.js
+++ models/index.js
@@ -1,13 +1,50 @@
 const User = require( './User' );
 const Post = require( './Post' );
+const Vote = require( './Vote' );
 
 // create associations
+    // one to many
 User.hasMany( Post, {
     foreignKey: 'user_id'
 } );
 
+    // one to many
 Post.belongsTo( User, {
     foreignKey: 'user_id',
 } );
 
-module.exports = { User, Post };
+    // many to many
+User.belongsToMany( Post, {
+    through: Vote,
+    as: 'voted_posts',
+    foreignKey: 'user_id'
+} );
+
+    // many to many
+Post.belongsToMany( User, {
+    through: Vote,
+    as: 'voted_posts',
+    foreignKey: 'post_id'
+} );
+
+    // one to many
+Vote.belongsTo( User, {
+    foreignKey: 'user_id'
+} );
+
+    // one to many
+Vote.belongsTo( Post, {
+    foreignKey: 'post_id'
+} );
+
+    // one to many
+User.hasMany( Vote, {
+    foreignKey: 'user_id'
+} );
+
+    // one to many
+Post.hasMany( Vote, {
+    foreignKey: 'post_id'
+} );
+
+module.exports = { User, Post, Vote };
