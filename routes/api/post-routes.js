a4f8b9b0f15a44065ed6365987019cc1f3fd607d
Andrew Ogilvie <primalorb@gmail.com>
2021-05-12 21:38:07 -0400

    add comment functions

--- routes/api/post-routes.js
+++ routes/api/post-routes.js
@@ -1,5 +1,5 @@
 const router = require( 'express' ).Router();
-const { Post, User, Vote } = require( '../../models' );
+const { Post, User, Vote, Comment } = require( '../../models' );
 const sequelize = require( '../../config/connection' );
 
 // get all posts
@@ -12,13 +12,21 @@ router.get( '/', ( req, res ) => {
             'created_at',
             [ sequelize.literal( '(SELECT COUNT(*) FROM vote WHERE post.id = vote.post_id)' ), 'vote_count' ]      
         ],
-        order: [ [ 'created_at', 'DESC' ] ],
         include: [
             {
+                model: Comment,
+                attributes: ['id', 'comment_text', 'post_id', 'user_id', 'created_at'],
+                include: {
+                    model: User,
+                    attributes: ['username']
+                }
+            },
+            {
                 model: User,
-                attributes: [ 'username' ]
+                attributes: ['username']
             }
-        ]
+        ],
+        order: [ [ 'created_at', 'DESC' ] ],
     } )
     .then( dbPostData => res.json( dbPostData ) )
     .catch( err => {
